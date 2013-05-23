9d62f69ff0927c1210b3060791f7c876da38c31a
Lionel Cons <lionel.cons@cern.ch>
2013-05-23 12:56:56 +0200

    improved the pylint wrapper to count errors

--- python/pylint.sh
+++ python/pylint.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 #
-# pylint wrapper
+# pylint wrapper couting errors and checking rating
 #
-status=0
-metrics=0
-pylint -f parseable ${1+"$@"} | while read line; do
-    if [ ${metrics} -eq 0 ]; then
-        if [ "x${line}" = "x" ]; then
-            metrics=1
-        else
+pylint -f parseable ${1+"$@"} | {
+    errors=0
+    metrics=0
+    while read line; do
+        if [ ${metrics} -eq 0 ]; then
+            if [ "x${line}" = "x" ]; then
+                metrics=1
+                continue
+            fi
+            case "${line}" in
+                *:\ \[[EF]*) errors=$((errors+1));;
+            esac
             echo "pylint: ${line}"
+        else
+            case "${line}" in
+                *rated\ at*)
+                    line=`echo ${line} | sed -e 's/ (.*)//'`
+                    echo -n "pylint: ${line} - "
+                    case "${line}" in
+                        *at\ 9*) echo "good";;
+                        *at\ 10*) echo "perfect";;
+                        *) echo "bad"; errors=$((errors+1));;
+                    esac
+                    ;;
+            esac
         fi
-    else
-        case "${line}" in
-            *rated*) echo "pylint: ${line}" | sed -e 's/ (.*)//';;
-        esac
-    fi
-done
-# FIXME: exit -1 if errors or rating < limit
-exit ${status}
+    done
+    echo "pylint: ${errors} errors detected"
+    [ ${errors} -gt 0 ] && exit -1
+}
+# FIXME: force Travis errors with "exit $?" once the code has been cleaned...
+exit 0
