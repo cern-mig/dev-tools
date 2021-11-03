ad014e7924937f1a821424e7f1b76560fbe2fea8
Lionel Cons <lionel.cons@cern.ch>
2021-11-03 11:59:27 +0100

    now using twine for PyPI uploading

--- python/release-python.sh
+++ python/release-python.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-PACKAGE_NAME=`basename ${PWD}` || { echo "error getting the package nam"; exit 1; }
-[ "x${PACKAGE_NAME}" == "x" ] && { echo "could not find a valid package name"; exit 1; }
+PACKAGE_NAME=`basename ${PWD}` || { echo "error getting the package name"; exit 1; }
+[ "x${PACKAGE_NAME}" = "x" ] && { echo "could not find a valid package name"; exit 1; }
 NAME="${PACKAGE_NAME#python-}"
 NAME=${NAME/-/.}
-[ "x${PACKAGE_NAME}" == "x" ] && { echo "error getting the module name"; exit 1; }
+[ "x${PACKAGE_NAME}" = "x" ] && { echo "error getting the module name"; exit 1; }
 
 echo "package name detected: ${PACKAGE_NAME}"
 echo "python module detected: ${NAME}"
@@ -14,11 +14,11 @@ import ${NAME}
 print(${NAME}.__version__)
 DELIM` || { echo "error getting module ${PACKAGE_NAME} version"; exit 1; }
 
-[ "x${VERSION}" == "x" ] && { echo "could not find a valid version"; exit 1; }
+[ "x${VERSION}" = "x" ] && { echo "could not find a valid version"; exit 1; }
 
 echo "going to release ${PACKAGE_NAME}-${VERSION}"
 read -p "do you want to proceed with the release? (y/n) " RESP
-if [ "x$RESP" == "xy" ]; then
+if [ "x$RESP" = "xy" ]; then
     echo "continuing"
 else
     echo "quitting"
@@ -27,11 +27,13 @@ fi
 
 TAGNAME="v${VERSION}"
 TAGEXISTS=`git tag -l | grep "^${TAGNAME}"`
-if [ "${TAGEXISTS}x" == "x" ]; then
-  echo "tagging current version with tag: ${TAGNAME}"
-  git tag ${TAGNAME}
+if [ "${TAGEXISTS}x" = "x" ]; then
+    echo "tagging current version with tag: ${TAGNAME}"
+    git tag ${TAGNAME}
+    echo "pushing tag remotely"
+    git push --tags
 else
-  echo "
+    echo "
 error: tag ${TAGNAME} already exists
 if you want to recreate it, delete it first:
     # remove local tag
@@ -39,7 +41,7 @@ if you want to recreate it, delete it first:
     # to remove it remotely
     git push origin :refs/tags/${TAGNAME}"
     read -p "do you want to proceed with the current tag? (y/n) " RESP
-    if [ "x$RESP" == "xy" ]; then
+    if [ "x$RESP" = "xy" ]; then
         echo "continuing"
     else
         echo "quitting"
@@ -47,11 +49,14 @@ if you want to recreate it, delete it first:
     fi
 fi
 
-echo "pushing tag remotely"
-git push --tags
-
 TMPDIR=`mktemp -d /tmp/${PACKAGE_NAME}.XXXXXX` || exit 1
 echo "temporary folder created: ${TMPDIR}"
+
+mkdir -p ${TMPDIR}/virtualenv
+virtualenv -p python3 ${TMPDIR}/virtualenv
+source ${TMPDIR}/virtualenv/bin/activate
+pip install --upgrade pip
+pip install --upgrade setuptools twine
 
 pushd ${TMPDIR}
 
@@ -63,13 +68,13 @@ pushd ${PACKAGE_NAME}
 echo "cleaning eventual py[co] files"
 find . -type f -name "*.py[co]" -exec rm -fv {} \;
 pycache=`find . -type d -name "__pycache__"`
-[ -n "${pycache}"] && rmdir -v ${pycache}
+[ "x${pycache}" = "x" ] || rmdir -v ${pycache}
 
-echo "executing python setup.py build"
-python setup.py	build
+echo "executing python setup.py sdist"
+python setup.py	sdist
 
-echo "uploading it to pypi"
-python setup.py sdist upload -r pypi || { echo "pypi upload failed"; exit 1; }
+echo "executing twine upload dist/*"
+twine upload dist/*
 
 popd # cloned folder
 popd # tmpdir
@@ -78,4 +83,3 @@ echo "removing temp dir ${TMPDIR}"
 rm -rf ${TMPDIR}
 
 echo "done!"
-
